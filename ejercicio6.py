b76404d289304a27ebabe72ff2e03accbad6111d
Lorrocarro <andres8023@hotmail.es>
2024-06-22 16:05:03 -0500

    ejrcicio 6 resulto

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,10 +1,10 @@
 
-6.("python")
+("python")
 x = 10
 y = 5
 if x > y:
     print("x es mayor que y")
 elif  x > y:
-        print("y es mayor que x")
+    print("y es mayor que x")
 else:
-        print("x es igual a y")
+    print("x es igual a y")
