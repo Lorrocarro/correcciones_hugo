c52c1db93fc87459a831eb2bafc7f505880279a5
Lorrocarro <andres8023@hotmail.es>
2024-06-24 08:56:48 -0500

    ejercicio 14 resuelto

--- ejercicio14.py
+++ ejercicio14.py
@@ -1,6 +1,6 @@
 def calcular_promedio(notas):
-    total = sum(notas)
-    promedio = total / len(notas)
+    total = (notas)
+    promedio = total = (notas)
     return promedio
 
 notas = [85, 92, 78, 91, 88]
